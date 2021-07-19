d70a48f134917e4ab8c459796cd4546e1028eaac
adriton <adrtler@gmail.com>
2021-07-19 20:01:26 +0200

    Flatlist

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import {HomeScreen} from '../screens/HomeScreen';
+import {View} from 'react-native';
 
 const Stack = createStackNavigator();
 
 export const Navigation = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen
+        name="HomeScreen"
+        options={{header: () => <></>}}
+        component={HomeScreen}
+      />
     </Stack.Navigator>
   );
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+interface MenuItem {
+  name: string;
+  icon: string;
+  component: string;
+}
+const menuItems = [
+  {
+    name: 'Animation 101',
+    icon: 'cube-outline',
+    component: 'Animation101Screen',
+  },
+];
+
 export const HomeScreen = () => {
+  const renderMenuItem = (menuItem: MenuItem) => {
+    return (
+      <View>
+        <Text>{menuItem.name}</Text>
+      </View>
+    );
+  };
   return (
-    <View>
-      <Text>HomeScreen</Text>
-      <Icon name="rocket" size={30} color="#900" />
+    <View style={styles.container}>
+      <FlatList
+        data={menuItems}
+        renderItem={({item}) => renderMenuItem(item)}
+        keyExtractor={item => item.name}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: 'green'},
+});
