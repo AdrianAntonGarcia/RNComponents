b6002bd2c62aa096bd93ae2a5dcc409899516ff0
adriton <adrtler@gmail.com>
2021-07-25 19:19:30 +0200

    FadeIn y FadeOut

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import {useRef} from 'react';
-import {View, StyleSheet, Animated} from 'react-native';
+import {View, StyleSheet, Animated, Button, Text} from 'react-native';
 
 export const Animation101Screen = () => {
   const opacity = useRef(new Animated.Value(0.4)).current;
 
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => console.log('El fadeIn terminó'));
+  };
+
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => console.log('El fadeOut terminó'));
+  };
+
   return (
     <View style={styles.container}>
-      <Animated.View style={{...styles.purpleBox, opacity}} />
+      <Animated.View style={{...styles.purpleBox, opacity, marginBottom: 20}} />
+      <Button title="FadeIn" onPress={fadeIn} />
+      <Button title="FadeOut" onPress={fadeOut} />
     </View>
   );
 };
