59da7af92323cbc1001f133b2c1ccb014e789c22
adriton <adrtler@gmail.com>
2021-07-25 18:33:20 +0200

    Flatlist separator y Header

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {FlatList, StyleSheet, Text, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/Ionicons';
+import {appStyles} from '../theme/appTheme';
 
 interface MenuItem {
   name: string;
@@ -13,14 +15,35 @@ const menuItems = [
     icon: 'cube-outline',
     component: 'Animation101Screen',
   },
+  {
+    name: 'Animation 102',
+    icon: 'albums-outline',
+    component: 'Animation102Screen',
+  },
 ];
 
 export const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
   const renderMenuItem = (menuItem: MenuItem) => {
     return (
       <View>
-        <Text>{menuItem.name}</Text>
+        <Text>
+          {menuItem.name} - {menuItem.icon}
+        </Text>
       </View>
+    );
+  };
+  const renderListHeader = () => {
+    return (
+      <View style={{marginTop: top + 20, marginBottom: 20}}>
+        <Text style={appStyles.title}>Opciones de Menú</Text>
+      </View>
+    );
+  };
+
+  const itemSeparator = () => {
+    return (
+      <View style={{borderBottomWidth: 1, opacity: 0.4, marginVertical: 5}} />
     );
   };
   return (
@@ -29,11 +52,13 @@ export const HomeScreen = () => {
         data={menuItems}
         renderItem={({item}) => renderMenuItem(item)}
         keyExtractor={item => item.name}
+        ListHeaderComponent={() => renderListHeader()}
+        ItemSeparatorComponent={itemSeparator}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {flex: 1, backgroundColor: 'green'},
+  container: {flex: 1, ...appStyles.globalMargin},
 });
