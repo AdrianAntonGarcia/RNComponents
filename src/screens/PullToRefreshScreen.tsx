3c18e71172a38cc86fdf333ad9bd8d28ae547941
adriton <adrtler@gmail.com>
2021-10-18 17:09:46 +0200

    Personalizando el refresh control

--- src/screens/PullToRefreshScreen.tsx
+++ src/screens/PullToRefreshScreen.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import {Text, View, ScrollView, RefreshControl} from 'react-native';
+import {View, ScrollView, RefreshControl} from 'react-native';
 import {HeaderTitle} from '../components/HeaderTitle';
 import {appStyles} from '../theme/appTheme';
 import {useState} from 'react';
 
 export const PullToRefreshScreen = () => {
   const [refreshing, setRefreshing] = useState(false);
+  const [data, setData] = useState<string>();
   const onRefresh = () => {
     setRefreshing(true);
     setTimeout(() => {
       console.log('Terminamos');
       setRefreshing(false);
-    }, 1500);
+      setData('Hola Mundo');
+    }, 3000);
   };
   return (
     <ScrollView
       refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          progressViewOffset={10}
+          progressBackgroundColor="#5856D6"
+          colors={['white', 'red', 'orange']}
+          style={{backgroundColor: '#5856D6'}}
+          tintColor="white"
+        />
       }>
       <View style={appStyles.globalMargin}>
         <HeaderTitle title="Pull to refresh" />
+        {data && <HeaderTitle title={data} />}
       </View>
     </ScrollView>
   );
