3aa704e831d0be80bb98d11a3683cb41ea15a995
adriton <adrtler@gmail.com>
2021-10-27 20:15:19 +0200

    refactorizacion navigation

--- App.tsx
+++ App.tsx
@@ -1,41 +1,12 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  NavigationContainer,
-  DefaultTheme,
-  DarkTheme,
-  useNavigationContainerRef,
-  Theme,
-} from '@react-navigation/native';
 import {Navigation} from './src/navigation/navigation';
-import {useReduxDevToolsExtension} from '@react-navigation/devtools';
 import {ThemeProvider} from './src/context/themeContext/ThemeContext';
 
-// const customTheme: Theme = {
-//   dark: true,
-//   colors: {
-//     ...DefaultTheme.colors,
-//     // primary: 'string',
-//     // background: 'string',
-//     // card: 'string',
-//     // text: 'string',
-//     // border: 'string',
-//     // notification: 'string',
-//   },
-// };
-
 export const App = () => {
-  const navigationRef = useNavigationContainerRef();
-
-  useReduxDevToolsExtension(navigationRef);
   return (
     <AppState>
-      <NavigationContainer
-        ref={navigationRef}
-        // theme={customTheme}
-      >
-        <Navigation />
-      </NavigationContainer>
+      <Navigation />
     </AppState>
   );
 };
--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -12,76 +12,88 @@ import {ModalScreen} from '../screens/ModalScreen';
 import {InfiniteScrollScreen} from '../screens/InfiniteScrollScreen';
 import {SlidesScreen} from '../screens/SlidesScreen';
 import {ChangeThemeScreen} from '../screens/ChangeThemeScreen';
-
+import {
+  NavigationContainer,
+  useNavigationContainerRef,
+} from '@react-navigation/native';
+import {useReduxDevToolsExtension} from '@react-navigation/devtools';
 const Stack = createStackNavigator();
 
 export const Navigation = () => {
+  const navigationRef = useNavigationContainerRef();
+
+  useReduxDevToolsExtension(navigationRef);
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-        // cardStyle: {backgroundColor: 'white'},
-      }}>
-      <Stack.Screen
-        name="HomeScreen"
-        options={{header: () => <></>}}
-        component={HomeScreen}
-      />
-      <Stack.Screen
-        name="Animation101Screen"
-        options={{header: () => <></>}}
-        component={Animation101Screen}
-      />
-      <Stack.Screen
-        name="Animation102Screen"
-        options={{header: () => <></>}}
-        component={Animation102Screen}
-      />
-      <Stack.Screen
-        name="SwitchScreen"
-        options={{header: () => <></>}}
-        component={SwitchScreen}
-      />
-      <Stack.Screen
-        name="AlertScreen"
-        options={{header: () => <></>}}
-        component={AlertScreen}
-      />
-      <Stack.Screen
-        name="TextInputScreen"
-        options={{header: () => <></>}}
-        component={TextInputScreen}
-      />
-      <Stack.Screen
-        name="PullToRefreshScreen"
-        options={{header: () => <></>}}
-        component={PullToRefreshScreen}
-      />
-      <Stack.Screen
-        name="CustomSectionListScreen"
-        options={{header: () => <></>}}
-        component={CustomSectionListScreen}
-      />
-      <Stack.Screen
-        name="ModalScreen"
-        options={{header: () => <></>}}
-        component={ModalScreen}
-      />
-      <Stack.Screen
-        name="InfiniteScrollScreen"
-        options={{header: () => <></>}}
-        component={InfiniteScrollScreen}
-      />
-      <Stack.Screen
-        name="SlidesScreen"
-        options={{header: () => <></>}}
-        component={SlidesScreen}
-      />
-      <Stack.Screen
-        name="ChangeThemeScreen"
-        options={{header: () => <></>}}
-        component={ChangeThemeScreen}
-      />
-    </Stack.Navigator>
+    <NavigationContainer
+      ref={navigationRef}
+      // theme={customTheme}
+    >
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          // cardStyle: {backgroundColor: 'white'},
+        }}>
+        <Stack.Screen
+          name="HomeScreen"
+          options={{header: () => <></>}}
+          component={HomeScreen}
+        />
+        <Stack.Screen
+          name="Animation101Screen"
+          options={{header: () => <></>}}
+          component={Animation101Screen}
+        />
+        <Stack.Screen
+          name="Animation102Screen"
+          options={{header: () => <></>}}
+          component={Animation102Screen}
+        />
+        <Stack.Screen
+          name="SwitchScreen"
+          options={{header: () => <></>}}
+          component={SwitchScreen}
+        />
+        <Stack.Screen
+          name="AlertScreen"
+          options={{header: () => <></>}}
+          component={AlertScreen}
+        />
+        <Stack.Screen
+          name="TextInputScreen"
+          options={{header: () => <></>}}
+          component={TextInputScreen}
+        />
+        <Stack.Screen
+          name="PullToRefreshScreen"
+          options={{header: () => <></>}}
+          component={PullToRefreshScreen}
+        />
+        <Stack.Screen
+          name="CustomSectionListScreen"
+          options={{header: () => <></>}}
+          component={CustomSectionListScreen}
+        />
+        <Stack.Screen
+          name="ModalScreen"
+          options={{header: () => <></>}}
+          component={ModalScreen}
+        />
+        <Stack.Screen
+          name="InfiniteScrollScreen"
+          options={{header: () => <></>}}
+          component={InfiniteScrollScreen}
+        />
+        <Stack.Screen
+          name="SlidesScreen"
+          options={{header: () => <></>}}
+          component={SlidesScreen}
+        />
+        <Stack.Screen
+          name="ChangeThemeScreen"
+          options={{header: () => <></>}}
+          component={ChangeThemeScreen}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 };
