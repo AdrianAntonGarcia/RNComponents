658985ac37ed7fb3ca23d726e84d128bcaa9fe96
adriton <adrtler@gmail.com>
2021-10-27 21:42:13 +0200

    Cambiando tema segun configuración del dispositivo

--- src/context/themeContext/ThemeContext.tsx
+++ src/context/themeContext/ThemeContext.tsx
@@ -1,5 +1,6 @@
-import React, {createContext, useReducer} from 'react';
-import {ThemeState, themeReducer, lightTheme} from './ThemeReducer';
+import React, {createContext, useEffect, useReducer} from 'react';
+import {Appearance, AppState, useColorScheme} from 'react-native';
+import {ThemeState, themeReducer, lightTheme, darkTheme} from './ThemeReducer';
 
 interface ThemeContextProps {
   theme: ThemeState;
@@ -10,13 +11,29 @@ interface ThemeContextProps {
 export const ThemeContext = createContext({} as ThemeContextProps);
 
 export const ThemeProvider = ({children}: any) => {
+  const colorScheme = useColorScheme();
   const [theme, dispatch] = useReducer(themeReducer, lightTheme);
+
+  // Solo en IOS
+  // useEffect(() => {
+  //   colorScheme === 'light' ? setLightTheme() : setDarkTheme();
+  // }, [colorScheme]);
   const setDarkTheme = () => {
     dispatch({type: 'set_dark_theme'});
   };
   const setLightTheme = () => {
     dispatch({type: 'set_light_theme'});
   };
+  useEffect(() => {
+    AppState.addEventListener('change', status => {
+      if (status === 'active') {
+        // when debugger is active you will always get "light" theme. Just close debugger and everything will be good.
+        Appearance.getColorScheme() === 'light'
+          ? setLightTheme()
+          : setDarkTheme();
+      }
+    });
+  }, []);
   return (
     <ThemeContext.Provider value={{theme, setLightTheme, setDarkTheme}}>
       {children}
